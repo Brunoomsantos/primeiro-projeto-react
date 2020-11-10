d509e41f4fc00fa90f9ada1e6dfa7c092f7231db
brunoomsantos <Bb030996>
2020-11-10 10:13:20 -0300

    Estilizando Detalhe - Incompleta

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { FiChevronLeft } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
+
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <>
+        <Header>
+            <img src={logoImg} alt="Github Explorer" />
+            <Link to="/">
+                <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+        </Header>
+
+        <RepositoryInfo>
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="Rocketseat" />
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>Descrição do repositório</p>
+                </div>
+            </header>
+            <ul>
+                <li>
+                    <strong>1808</strong>
+                    <span>Stars</span>
+                </li>
+                <li>
+                    <strong>48</strong>
+                    <span>Forks</span>
+                </li>
+                <li>
+                    <strong>67</strong>
+                    <span>Issues abertas</span>
+                </li>
+            </ul>
+        </RepositoryInfo>
+        </>
+    );
 };
 
 export default Repository;
